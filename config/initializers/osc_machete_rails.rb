c214d26cf8890bba2317bc5ee920f9a5ca7fbe96
Eric Franz <efranz@osc.edu>
2017-06-29 22:03:27 -0400

    fix title

--- config/initializers/osc_machete_rails.rb
+++ config/initializers/osc_machete_rails.rb
@@ -1,3 +1,3 @@
 OscMacheteRails.update_status_of_all_active_jobs_on_each_request = false
 
-Rails.configuration.x.title = YAML.load('manifest.yml')['name']
+Rails.configuration.x.title = YAML.load(File.read('manifest.yml'))['name']
