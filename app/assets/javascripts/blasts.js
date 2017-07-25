5208eb1220a5ecbb2c8a9a934b592a5224d041c1
Eric Franz <efranz@osc.edu>
2017-07-25 13:53:38 -0400

    add selection of svg nodes using css class

--- app/assets/javascripts/blasts.js
+++ app/assets/javascripts/blasts.js
@@ -4,20 +4,34 @@ function genfdgraph(svg, graph){
       width = svg._groups[0][0].scrollWidth,
       height = svg._groups[0][0].scrollHeight;
 
+  var updateSelectedNode = function(selected_node, all_nodes, d){
+    // deselect previous selected nodes
+    all_nodes.classed("selected", false);
+
+    // select new node
+    selected_node.classed("selected", true);
+    console.log(d);
+
+    // TODO: update selected_node data view
+  };
+
   var dragstarted = function(d) {
     if (!d3.event.active) simulation.alphaTarget(0.3).restart();
     d.fx = d.x;
     d.fy = d.y;
-  }
+
+    updateSelectedNode(d3.select(this), node, d);
+  };
+
   var dragged = function(d) {
     d.fx = d3.event.x;
     d.fy = d3.event.y;
-  }
+  };
   var dragended = function(d) {
     if (!d3.event.active) simulation.alphaTarget(0);
     d.fx = null;
     d.fy = null;
-  }
+  };
 
   //update display
   svg.selectAll().remove();
@@ -34,6 +48,7 @@ function genfdgraph(svg, graph){
       .append("line")
       .attr("stroke-width", function(d) { return Math.sqrt(d.weight/100); });
 
+
   var node = svg.append("g")
       .attr("class", "nodes")
       .selectAll("circle")
@@ -43,6 +58,9 @@ function genfdgraph(svg, graph){
       .attr("r", 10)
       .style("fill-opacity", .5)
       .attr("fill", d3.rgb('#ff8282'))//function(d) { return color(d.group); })
+      .on("click", function(d){
+        updateSelectedNode(d3.select(this), node, d);
+      })
       .call(d3.drag()
         .on("start", dragstarted)
         .on("drag", dragged)
