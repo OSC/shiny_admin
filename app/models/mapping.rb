510017594feb5b82a9cc62d185b80c4335f52160
Morgan Rodgers <mrodgers@osc.edu>
2018-07-06 15:52:45 -0400

    Implement permissions check for Mapping::is_still_valid?
    Permissions may change or files may be deleted outside of this app, ensure that we can detect when a breaking change has occurred.

--- app/models/mapping.rb
+++ app/models/mapping.rb
@@ -10,10 +10,8 @@ class Mapping < ActiveRecord::Base
 
 
   def is_still_valid?
-
-    app_exists = File.directory?(File.join(ENV['BSIR_PROJECT_SPACE'], app))
+    app_exists = File.directory?(app_full_path)
     dataset_exists = File.directory?(dataset)
-    user_has_permissions_on_both = true  # TODO
     
     return app_exists && dataset_exists && user_has_permissions_on_both
   end
@@ -33,6 +31,11 @@ class Mapping < ActiveRecord::Base
   end
 
 
+  def app_full_path
+    File.join(ENV['BSIR_PROJECT_SPACE'], app)
+  end
+
+
   def to_hash
     {:app => app, :user => user, :dataset => dataset, :extensions => extensions}
   end
@@ -45,5 +48,36 @@ class Mapping < ActiveRecord::Base
 
   after_destroy do |mapping|
     Mapping.dump_to_yaml
+  end
+
+
+  private
+
+  # Check whether a user has read/execute permissions on the app and dataset directories
+  # @return [Boolean] does user ahave correct permissions?
+  def user_has_permissions_on_both
+    ood_user = OodSupport::User.new(user)
+    required_permissions = [:r, :x]
+    
+    app_acl = OodSupport::ACLs::Nfs4ACL.get_facl(path: app_full_path)
+    dataset_acl = nil
+    begin
+      dataset_acl = OodSupport::ACLs::Nfs4ACL.get_facl(path: dataset)
+    rescue OodSupport::InvalidPath
+      return false
+    end
+
+    for required_permission in required_permissions do
+      unless app_acl.allow?(principle: ood_user, permission: required_permission)
+        return false
+      end
+
+      unless dataset_acl.allow?(principle: ood_user, permission: required_permission)
+        return false
+      end
+    end
+
+    # Everything went well so return true
+    return true
   end
 end
