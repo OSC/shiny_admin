b58829d8e9b994b3d03c8d8fe4d97db55bf6ea79
Morgan Rodgers <mrodgers@osc.edu>
2018-09-11 11:21:52 -0400

    Change way we build list of managed datasets #27

--- app/helpers/mappings_helper.rb
+++ app/helpers/mappings_helper.rb
@@ -29,14 +29,25 @@ module MappingsHelper
     app_path.basename
   end
 
+  def installed_datasets
+    results = []
+    Pathname.new(Configuration.app_dataset_root).find do |path|
+      next unless path.directory?
+
+      if path.children.any?{|path| path.basename.to_s.downcase == 'data.rds'}
+        results << path
+        Find.prune
+      end
+    end
+
+    results.sort
+  end
 
   # Get a list of datasets
   # The list will consist of datasets detected at the configured location and
   # datasets that have been saved to the database but are stored in nonstandard
   # locations.
   def known_datasets
-    installed_datasets = Dir.glob(Configuration.app_dataset_root.join('*')).sort
-
     # Take the union of the two sets of datasets
     installed_datasets | Mapping.datasets
   end
