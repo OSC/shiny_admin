9ee110d9e5c47038bb7e3d89584a4f4c7344a6aa
Eric Franz <efranz@osc.edu>
2018-11-28 20:43:36 -0500

    rm superfluous to_a calls

--- app/models/managed_shared_file.rb
+++ app/models/managed_shared_file.rb
@@ -104,7 +104,7 @@ class ManagedSharedFile
   # @param datasets [Array<Pathname>] - installed_datasets(Configuration.app_dataset_root) to determine which paths under dataset_root are actual datasets
   # @return [Array<ChangeReport>] array of report objects for each path that was updated or each error
   def fix_dataset_root_permissions(dataset_root, datasets)
-    dataset_root.find.to_a.map { |path|
+    dataset_root.find.map { |path|
       if datasets.include?(path)
         fix_facl path, dataset_acl_template(path)
       elsif path.directory?
@@ -148,6 +148,6 @@ class ManagedSharedFile
   end
 
   def fix_dataset_root_group_ownership(dataset_root, group)
-    fix_group_ownership_for_files dataset_root.find.to_a, group
+    fix_group_ownership_for_files dataset_root.find, group
   end
 end
